d20ca0c8d2181337c711a11038e604366ce95f30
t03jam8 <james.margrove@usys.ethz.ch>
2018-01-26 15:57:23 +0100

    overall effect of water inundation on dipterocarp mortality

--- MortalityAnalysis.R
+++ MortalityAnalysis.R
@@ -1,7 +1,19 @@
-#####
-#####
-#####
-#####
+##### Paper: water inundation sensitivity in dipterocarps
+##### author: james margrove 
+##### date: 26.01.2017
+
+##### imported packaged 
+require(ggplot2)
+require(lme4)
 
 data <- read.table("Experiment, mort, leafAB, dden, wden,sla.txt", header = TRUE);
 str(data)
+
+##### data exploration with ggplot. 
+ggplot(data, aes(x=treat, y=surv)) + geom_point() + stat_smooth()
+
+model1 <- glmer(surv ~ treat + dia + (1|sp/mother) + (1|block), data = data, family = "binomial")
+summary(model1)
+
+##### effect
+# the effect of flooding overall on dipterocarps is negative. 
