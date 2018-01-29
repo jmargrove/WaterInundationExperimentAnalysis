08525886f7b8356bc9a4b33e43dbb916bc75b3b1
t03jam8 <james.margrove@usys.ethz.ch>
2018-01-29 11:10:46 +0100

    feat: max and min probs for mortality in Dipterocarps

--- MortalityAnalysis.R
+++ MortalityAnalysis.R
@@ -5,6 +5,8 @@
 ##### imported packaged 
 require(ggplot2)
 require(lme4)
+source("./functions/newbinplot.R"); require(arm)
+source("./functions/ranNorm.R")
 
 data <- read.table("Experiment, mort, leafAB, dden, wden,sla.txt", header = TRUE);
 str(data)
@@ -17,3 +19,27 @@ summary(model1)
 
 ##### effect
 # the effect of flooding overall on dipterocarps is negative. 
+##### model validation.
+
+##### residuals & fitted values 
+residuals <- resid(model1, type =  "pearson");
+fitted <- fitted(model1)
+
+# Some deviations from normallity but otherwise OK 
+newbinplot(residuals, fitted);
+
+# Random effects reasonabbly normally distributed
+ranNorm("mother:sp", slope = 1, model = model1)
+ranNorm("sp", slope = 1, model = model1)
+ranNorm("block", slope = 1, model = model1)
+
+##### Graphing the analysis
+preds <- expand.grid(dia = mean(data$dia), treat = seq(from = 0, to = 21, length = 100))
+preds$p = predict(model1, type = "response", newdata = preds, re.form = NA)
+head(preds)
+
+ggplot(preds, aes(x=treat, y=p)) + geom_line() 
+
+##### extremes of survival curve....
+min(preds$p)
+max(preds$p)
